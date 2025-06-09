599a332c580d76b24f666abdaf9e11469ef732ce
JoseR <167713472+SirJosesito@users.noreply.github.com>
2025-06-09 01:18:55 -0300

    implementacion de csv (nofinal)
    añadi el csv

--- backend/src/encuestas/encuestas.controller.ts
+++ backend/src/encuestas/encuestas.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Response } from '@nestjs/common';
 import { EncuestasService } from './encuestas.service';
 import { CreateEncuestasDto } from './dto/create-encuestas.dto';
 
@@ -32,5 +32,16 @@ export class EncuestasController {
   @Get('estadisticas/:codigo_resultados')
   findStatistics(@Param('codigo_resultados') codigo_resultados: string) {
     return this.encuestasService.obtenerEstadisticas(codigo_resultados);
+  }
+
+  @Get('estadisticas/csv/:codigo_resultados')
+    async exportEstadisticasToCsv(
+    @Param('codigo_resultados') codigo_resultados: string,
+    @Response() res,
+    ) {
+  const csv = await this.encuestasService.exportEstadisticasToCsv(codigo_resultados);
+  res.header('Content-Type', 'text/csv');
+  res.attachment(`estadisticas_${codigo_resultados}.csv`);
+  res.send(csv);
   }
 }
